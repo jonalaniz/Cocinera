25df0118183074484b38eb6838153e29a14cf4e6
Jon Alaniz <jonalaniz@gmail.com>
2023-02-15 10:05:43 -0600

    Added address bar

--- Cocinera/LoginFlow/LoginController.swift
+++ Cocinera/LoginFlow/LoginController.swift
@@ -24,9 +24,9 @@ class LoginController: UIViewController {
         view.layer.borderColor = UIColor(red: 114/255, green: 114/255, blue: 114/255, alpha: 0.5).cgColor
         view.layer.borderWidth = 1
         view.widthAnchor.constraint(equalToConstant: 340).isActive = true
-        view.heightAnchor.constraint(equalToConstant: 260).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 240).isActive = true
         
-        view.layer.cornerRadius = 28
+        view.layer.cornerRadius = 25
         view.clipsToBounds = true
         
         view.layer.masksToBounds = false
@@ -43,7 +43,7 @@ class LoginController: UIViewController {
         button.backgroundColor = .systemGreen
         button.setTitle("Log In", for: .normal)
         
-        button.layer.cornerRadius = 12
+        button.layer.cornerRadius = 10
         button.clipsToBounds = true
         
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -51,6 +51,28 @@ class LoginController: UIViewController {
         button.heightAnchor.constraint(equalToConstant: 44).isActive = true
         
         return button
+    }()
+    
+    let addressField: UITextField = {
+        let textField = UITextField()
+        textField.textContentType = .URL
+        textField.textColor = .white
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.keyboardType = .URL
+        textField.returnKeyType = .done
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.layer.borderColor = UIColor(red: 114/255, green: 114/255, blue: 114/255, alpha: 0.5).cgColor
+        textField.backgroundColor = UIColor(red: 39/255, green: 40/255, blue: 43/255, alpha: 1)
+        
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 46).isActive = true
+        
+        return textField
     }()
 
     override func viewDidLoad() {
@@ -67,6 +89,8 @@ class LoginController: UIViewController {
         view.addSubview(backgroundImageView)
         view.addSubview(loginView)
         loginView.addSubview(loginButton)
+        loginView.addSubview(addressField)
+        styleTextField(textField: addressField)
         
         NSLayoutConstraint.activate([
             backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -75,13 +99,24 @@ class LoginController: UIViewController {
             backgroundImageView.rightAnchor.constraint(equalTo: view.rightAnchor),
             
             loginView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
             
             loginButton.centerXAnchor.constraint(equalTo: loginView.centerXAnchor),
-            loginButton.bottomAnchor.constraint(equalTo: loginView.bottomAnchor, constant: -20)
+            loginButton.bottomAnchor.constraint(equalTo: loginView.bottomAnchor, constant: -20),
+            
+            addressField.centerXAnchor.constraint(equalTo: loginView.centerXAnchor),
+            addressField.bottomAnchor.constraint(equalTo: loginButton.topAnchor, constant: -30)
         ])
     }
+    
+    private func styleTextField(textField: UITextField) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textField.frame.height))
 
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
+        textField.layoutIfNeeded()
+
+    }
 
 }
 
